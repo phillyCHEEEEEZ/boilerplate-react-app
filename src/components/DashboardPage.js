9e3bf5fde5897414ddf587eb16f098f6e3988446
Philip Nacamuli <panac@nacamuli.net>
2018-12-16 15:08:24 -0600

    init commit

---
+++ src/components/DashboardPage.js
@@ -0,0 +1,11 @@
+/* eslint-disable */
+
+import React from 'react';
+
+const DashboardPage = () => (
+  <div>
+    Dashboard page content
+  </div>
+);
+
+export default DashboardPage;
---
+++ src/tests/components/DashboardPage.test.js
@@ -0,0 +1,11 @@
+/* eslint-disable */
+
+import React from 'react';
+import { shallow } from 'enzyme';
+import DashboardPage from '../../components/DashboardPage';
+
+test('should render DashboardPage correctly', () => {
+  const wrapper = shallow(<DashboardPage />);
+
+  expect(wrapper).toMatchSnapshot();
+});
